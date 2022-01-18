cc2455b9f2ddd7c786487b3da94beadc409c95ac
0x192 <mreve@keemail.me>
2022-01-18 04:20:24 +0100

    Forgot to run rust fmt
    I really should add a pre-commit hook for this

--- src/gui/views/list.rs
+++ src/gui/views/list.rs
@@ -515,10 +515,10 @@ impl List {
     ) -> Result<usize, ()> {
         match adb_shell_command(true, &action) {
             Ok(o) => {
-                // On old devices, adb commands can return the '0' exit code even if there 
-                // is an error. On Android 4.4, ADB doesn't check if the package exists. 
+                // On old devices, adb commands can return the '0' exit code even if there
+                // is an error. On Android 4.4, ADB doesn't check if the package exists.
                 // It does not return any error if you try to `pm block` a non-existent package.
-                // Some commands are even killed by ADB before finishing and UAD can't catch 
+                // Some commands are even killed by ADB before finishing and UAD can't catch
                 // the output.
                 if ["Error", "Failure"].iter().any(|&e| o.contains(e)) {
                     error!("[{}] {} -> {}", recommendation, action, o);
