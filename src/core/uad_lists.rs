0x192/universal-android-debloater

commit 9b564d3825dc4721773fe499273bbb30c1be10dd

refactor: major code overhaul + support for the new Iced theme API (#369)
Note: UAD is now temporarily based on my fork of Iced until iced-rs/iced#1425 is merged upstream.

- Remove custom code for theming: UAD now uses the new Iced styling API
- All the code for initializing UAD (download debloat lists, fetch connected devices etc...) is now a lot more streamlined
- Other miscellaneous code improvement
- Replace row() and column() functions by the new Iced vector-like helper functions (row![], column![])
- Move navigation menu GUI code in a dedicated file
- Rework the way UAD uses events for async code

src/core/uad_lists.rs
@@ -197,7 +197,7 @@ impl std::fmt::Display for Removal {
     }
 }
 
-pub async fn load_debloat_lists(remote: bool) -> (Result<HashMap<String, Package>, ()>, bool) {
+pub fn load_debloat_lists(remote: bool) -> (Result<HashMap<String, Package>, ()>, bool) {
     let cached_uad_lists: PathBuf = CACHE_DIR.join("uad_lists.json");
     let mut error = false;
     let list: Vec<Package> = if remote {

Cargo.toml
@@ -19,7 +19,7 @@ self-update = ["flate2", "tar"]
 no-self-update = []
 
 [dependencies]
-iced = { git = "https://github.com/hecrj/iced.git", features = ["pure"] }
+iced = { git = "https://github.com/0x192/iced.git", branch = "patch/theme" }
 serde = { version = "^1.0", features = ["derive"] }
 serde_json = "^1.0"
 static_init = "^1.0"

src/core/sync.rs
@@ -90,7 +90,7 @@ pub fn adb_shell_command(shell: bool, args: &str) -> Result<String, String> {
     }
 }
 
-pub fn list_all_system_packages(user_id: &Option<&User>) -> String {
+pub fn list_all_system_packages(user_id: Option<&User>) -> String {
     let action = match user_id {
         Some(user_id) => format!("pm list packages -s -u --user {}", user_id.id),
         None => "pm list packages -s -u".to_string(),
@@ -101,7 +101,7 @@ pub fn list_all_system_packages(user_id: &Option<&User>) -> String {
         .replace("package:", "")
 }
 
-pub fn hashset_system_packages(state: PackageState, user_id: &Option<&User>) -> HashSet<String> {
+pub fn hashset_system_packages(state: PackageState, user_id: Option<&User>) -> HashSet<String> {
     let user = match user_id {
         Some(user_id) => format!(" --user {}", user_id.id),
         None => "".to_string(),
@@ -137,10 +137,10 @@ pub fn action_handler(
             };
 
             match phone.android_sdk {
-                i if i >= 23 => commands,                // > Android Lollipop (6.0)
+                sdk if sdk >= 23 => commands,            // > Android Lollipop (6.0)
                 21 | 22 => vec!["pm hide", "pm clear"],  // Android Lollipop (5.x)
                 19 | 20 => vec!["pm block", "pm clear"], // Android KitKat (4.4/4.4W)
-                _ => vec!["pm uninstall"], // Disable mode is unavailable on older devices because the needed ADB commands need root
+                _ => vec!["pm uninstall"], // Disable mode is unavailable on older devices because the specific ADB commands need root
             }
         }
         PackageState::Uninstalled => {
@@ -217,7 +217,7 @@ pub fn get_user_list() -> Vec<User> {
 }
 
 // getprop ro.serialno
-pub async fn get_device_list() -> Vec<Phone> {
+pub async fn get_devices_list() -> Vec<Phone> {
     match retry(
         Fixed::from_millis(500).take(120),
         || match adb_shell_command(false, "devices") {

src/core/theme.rs
@@ -7,6 +7,12 @@ pub struct Theme {
     pub palette: ColorPalette,
 }
 
+impl Default for Theme {
+    fn default() -> Self {
+        Theme::lupin()
+    }
+}
+
 #[derive(Debug, Clone, Copy)]
 pub struct BaseColors {
     pub background: Color,

src/core/utils.rs
@@ -5,10 +5,8 @@ use crate::core::theme::Theme;
 use crate::core::uad_lists::{Package, PackageState, Removal, UadList};
 use crate::gui::views::list::Selection;
 use crate::gui::widgets::package_row::PackageRow;
-use crate::gui::ICONS;
 use chrono::offset::Utc;
 use chrono::DateTime;
-use iced::{alignment, Length, Text};
 use std::collections::HashMap;
 use std::fs;
 use std::io::{self, prelude::*, BufReader};
@@ -17,7 +15,7 @@ use std::process::Command;
 
 pub fn fetch_packages(
     uad_lists: &HashMap<String, Package>,
-    user_id: &Option<&User>,
+    user_id: Option<&User>,
 ) -> Vec<PackageRow> {
     let all_system_packages = list_all_system_packages(user_id); // installed and uninstalled packages
     let enabled_system_packages = hashset_system_packages(PackageState::Enabled, user_id);
@@ -120,14 +118,6 @@ pub fn import_selection(packages: &mut [PackageRow], selection: &mut Selection) 
     }
 
     Ok(())
-}
-
-pub fn icon(unicode: char) -> Text {
-    Text::new(&unicode.to_string())
-        .font(ICONS)
-        .width(Length::Units(17))
-        .horizontal_alignment(alignment::Horizontal::Center)
-        .size(17)
 }
 
 pub fn string_to_theme(theme: String) -> Theme {

commit 5dc7093c5c2151ff07ad5d25891a8dac22ed6278

fix: auto reload in offline mode after remote UAD list download timeout
In offline mode, after clicking on the `no internet` button, UAD would reload with an empty list after the end of the remote UAD lists download timeout.

Regression introduced in 9b564d3 (no public release affected)

src/core/uad_lists.rs
@@ -197,11 +197,12 @@ impl std::fmt::Display for Removal {
     }
 }
 
-pub fn load_debloat_lists(remote: bool) -> (Result<HashMap<String, Package>, ()>, bool) {
+type PackageHashMap = HashMap<String, Package>;
+pub fn load_debloat_lists(remote: bool) -> (Result<PackageHashMap, PackageHashMap>, bool) {
     let cached_uad_lists: PathBuf = CACHE_DIR.join("uad_lists.json");
     let mut error = false;
     let list: Vec<Package> = if remote {
-        match retry(Fixed::from_millis(500).take(120), || {
+        match retry(Fixed::from_millis(1000).take(60), || {
             match ureq::get(
                 "https://raw.githubusercontent.com/0x192/universal-android-debloater/\
             main/resources/assets/uad_lists.json",
@@ -222,7 +223,7 @@ pub fn load_debloat_lists(remote: bool) -> (Result<HashMap<String, Package>, ()>
             }
         }) {
             Ok(list) => list,
-            Err(_) => vec![],
+            Err(_) => get_local_lists(),
         }
     } else {
         warn!("Could not load remote debloat list");
@@ -236,7 +237,7 @@ pub fn load_debloat_lists(remote: bool) -> (Result<HashMap<String, Package>, ()>
         package_lists.insert(name, p);
     }
     if error {
-        (Err(()), remote)
+        (Err(package_lists), remote)
     } else {
         (Ok(package_lists), remote)
     }

src/gui/views/list.rs
@@ -102,13 +102,21 @@ impl List {
             }
             Message::LoadPhonePackages(list_box) => {
                 let (uad_list, list_state) = list_box;
-                self.loading_state = LoadingState::LoadingPackages;
-                self.uad_lists = uad_list.clone();
-                *list_update_state = list_state;
-                Command::perform(
-                    Self::load_packages(uad_list, selected_device.user_list.clone()),
-                    Message::ApplyFilters,
-                )
+
+                // The refresh button has been pushed or UAD has just been launched
+                if self.uad_lists.is_empty() {
+                    self.loading_state = LoadingState::LoadingPackages;
+                }
+                if *list_update_state != UadListState::Done {
+                    self.uad_lists = uad_list.clone();
+                    *list_update_state = list_state;
+                    Command::perform(
+                        Self::load_packages(uad_list, selected_device.user_list.clone()),
+                        Message::ApplyFilters,
+                    )
+                } else {
+                    Command::none()
+                }
             }
             Message::ApplyFilters(packages) => {
                 self.phone_packages = packages;
@@ -535,23 +543,18 @@ impl List {
         remote: bool,
         phone: Phone,
     ) -> (HashMap<String, Package>, UadListState) {
-        let (uad_lists, remote) = load_debloat_lists(remote);
+        let (uad_lists, _) = load_debloat_lists(remote);
         match uad_lists {
             Ok(list) => {
                 env::set_var("ANDROID_SERIAL", phone.adb_id.clone());
                 if phone.adb_id.is_empty() {
                     error!("AppsView ready but no phone found");
                 }
-
-                if !remote {
-                    (list, UadListState::Failed)
-                } else {
-                    (list, UadListState::Done)
-                }
+                (list, UadListState::Done)
             }
-            Err(_) => {
-                error!("Error loading debloat list for the phone");
-                (HashMap::new(), UadListState::Failed)
+            Err(local_list) => {
+                error!("Error loading remote debloat list for the phone. Fallback to embedded (and outdated) list");
+                (local_list, UadListState::Failed)
             }
         }
     }