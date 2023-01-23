0x192/universal-android-debloater

commit 5ea61774cb6d1b19bc257f428cbea2745baa09ea

fix: UAD crash when interacting with work profiles (#448)
On recent Android devices, you can't interact with the user of the work profile. Any adb commands involving this user will fail with a `Shell does not have permission to access user ${user}.`

UAD was wrongly assuming stuff and tried to access non-existent data, thus causing a crash.

src/core/sync.rs
@@ -225,6 +225,9 @@ pub fn apply_pkg_state_commands(
         _ => vec![],
     };
     request_builder(commands, &package.name, &[*selected_user])
+        .iter()
+        .map(|(_, command)| command.clone())
+        .collect()
 }
 
 pub fn action_handler(
@@ -232,7 +235,7 @@ pub fn action_handler(
     package: &CorePackage,
     phone: &Phone,
     settings: &DeviceSettings,
-) -> Vec<String> {
+) -> Vec<(Option<usize>, String)> {
     // https://github.com/0x192/universal-android-debloater/wiki/ADB-reference
     // ALWAYS PUT THE COMMAND THAT CHANGES THE PACKAGE STATE FIRST!
     let commands = match package.state {
@@ -275,20 +278,24 @@ pub fn action_handler(
     }
 }
 
-pub fn request_builder(commands: Vec<&str>, package: &str, users: &[User]) -> Vec<String> {
+pub fn request_builder(
+    commands: Vec<&str>,
+    package: &str,
+    users: &[User],
+) -> Vec<(Option<usize>, String)> {
     if !users.is_empty() {
         users
             .iter()
             .flat_map(|u| {
                 commands
                     .iter()
-                    .map(|c| format!("{} --user {} {}", c, u.id, package))
+                    .map(|c| (Some(u.index), format!("{} --user {} {}", c, u.id, package)))
             })
             .collect()
     } else {
         commands
             .iter()
-            .map(|c| format!("{} {}", c, package))
+            .map(|c| (None, format!("{} {}", c, package)))
             .collect()
     }
 }

src/gui/views/list.rs
@@ -25,6 +25,7 @@ pub struct Selection {
 
 #[derive(Debug, Default, Clone)]
 pub struct PackageInfo {
+    pub i_user: Option<usize>,
     pub index: usize,
     pub removal: String,
 }
@@ -225,8 +226,9 @@ impl List {
                             &settings.device,
                         );
 
-                        for (i, action) in actions.into_iter().enumerate() {
+                        for (i, (i_user, action)) in actions.into_iter().enumerate() {
                             let p_info = PackageInfo {
+                                i_user,
                                 index: i_package,
                                 removal: package.removal.to_string(),
                             };
@@ -277,10 +279,12 @@ impl List {
                         &settings.device,
                     );
 
-                    for (j, action) in actions.into_iter().enumerate() {
+                    let package = &mut self.phone_packages[i_user][i];
+                    for (j, (i_user, action)) in actions.into_iter().enumerate() {
                         let p_info = PackageInfo {
+                            i_user,
                             index: i,
-                            removal: self.phone_packages[i_user][i].removal.to_string(),
+                            removal: package.removal.to_string(),
                         };
                         // Only the first command can change the package state
                         commands.push(Command::perform(
@@ -312,17 +316,15 @@ impl List {
                     let package = &mut self.phone_packages[i_user][p.index];
                     update_selection_count(&mut self.selection, package.state, false);
 
-                    if !settings.device.multi_user_mode {
+                    if !settings.device.multi_user_mode || p.i_user.is_none() {
                         package.state = package.state.opposite(settings.device.disable_mode);
                         package.selected = false;
                     } else {
-                        for u in &selected_device.user_list {
-                            self.phone_packages[u.index][p.index].state = self.phone_packages
-                                [u.index][p.index]
-                                .state
-                                .opposite(settings.device.disable_mode);
-                            self.phone_packages[u.index][p.index].selected = false;
-                        }
+                        self.phone_packages[p.i_user.unwrap()][p.index].state = self.phone_packages
+                            [p.i_user.unwrap()][p.index]
+                            .state
+                            .opposite(settings.device.disable_mode);
+                        self.phone_packages[p.i_user.unwrap()][p.index].selected = false;
                     }
                     self.selection
                         .selected_packages

src/gui/views/settings.rs
@@ -142,6 +142,7 @@ impl Settings {
                     *nb_running_async_adb_commands = 0;
                     for p in &r_packages {
                         let p_info = PackageInfo {
+                            i_user: None,
                             index: p.index,
                             removal: "RESTORE".to_string(),
                         };

commit a73e0e5577d1c803c165dc35069c0e3d01f8162d

fix: package state not being updated after action
This fixes the regression introduced in 5ea6177 (no public release affected)

This commit also prevents UAD to try interacting with a protected user (to avoid error spam in the logfiles)

src/core/save.rs
@@ -88,7 +88,11 @@ pub fn list_available_backup_user(backup: DisplayablePath) -> Vec<User> {
 
             let mut users = vec![];
             for u in phone_backup.users {
-                users.push(User { id: u.id, index: 0 });
+                users.push(User {
+                    id: u.id,
+                    index: 0,
+                    protected: false,
+                });
             }
             users
         }

src/core/sync.rs
@@ -41,10 +41,11 @@ impl std::fmt::Display for Phone {
     }
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Copy)]
+#[derive(Default, Debug, Clone, PartialEq, Eq, Copy)]
 pub struct User {
     pub id: u16,
     pub index: usize,
+    pub protected: bool,
 }
 
 impl std::fmt::Display for User {
@@ -241,7 +242,7 @@ pub fn apply_pkg_state_commands(
 
 pub fn action_handler(
     selected_user: &User,
-    package: &CorePackage,
+    package: CorePackage,
     phone: &Phone,
     settings: &DeviceSettings,
 ) -> Vec<(Option<usize>, String)> {
@@ -295,6 +296,7 @@ pub fn request_builder(
     if !users.is_empty() {
         users
             .iter()
+            .filter(|&u| !u.protected)
             .flat_map(|u| {
                 commands
                     .iter()
@@ -340,6 +342,10 @@ pub fn get_phone_brand() -> String {
     )
 }
 
+pub fn is_protected_user(user_id: &str) -> bool {
+    adb_shell_command(true, &("pm list packages --user ".to_owned() + user_id)).is_err()
+}
+
 pub fn get_user_list() -> Vec<User> {
     #[dynamic]
     static RE: Regex = Regex::new(r"\{([0-9]+)").unwrap();
@@ -350,6 +356,7 @@ pub fn get_user_list() -> Vec<User> {
             .map(|(i, u)| User {
                 id: u.as_str()[1..].parse().unwrap(),
                 index: i,
+                protected: is_protected_user(&u.as_str()[1..]),
             })
             .collect(),
         Err(_) => vec![],

src/core/utils.rs
@@ -162,21 +162,3 @@ impl fmt::Display for DisplayablePath {
         write!(f, "{stem}")
     }
 }
-
-pub fn unavailable_users(user_list: &[User], packages: &[Vec<PackageRow>]) -> Vec<String> {
-    let mut unavailable_users = vec![];
-
-    for (u_index, p_list) in packages.iter().enumerate() {
-        if p_list.is_empty() {
-            unavailable_users.push(
-                user_list
-                    .iter()
-                    .find(|u| u.index == u_index)
-                    .unwrap()
-                    .id
-                    .to_string(),
-            );
-        }
-    }
-    unavailable_users
-}

src/gui/mod.rs
@@ -306,7 +306,7 @@ impl Application for UadGui {
                 .map(Message::AboutAction),
             View::Settings => self
                 .settings_view
-                .view(&self.apps_view.phone_packages, &selected_device)
+                .view(&selected_device)
                 .map(Message::SettingsAction),
         };
 

src/gui/views/list.rs
@@ -1,3 +1,4 @@
+use crate::core::config::DeviceSettings;
 use crate::core::sync::{action_handler, perform_adb_commands, CommandType, Phone, User};
 use crate::core::theme::Theme;
 use crate::core::uad_lists::{
@@ -94,7 +95,7 @@ impl List {
         list_update_state: &mut UadListState,
         message: Message,
     ) -> Command<Message> {
-        let i_user = self.selected_user.unwrap_or(User { id: 0, index: 0 }).index;
+        let i_user = self.selected_user.unwrap_or_default().index;
         match message {
             Message::RestoringDevice(output) => {
                 if let Ok(res) = output {
@@ -137,7 +138,7 @@ impl List {
                 self.selected_package_state = Some(PackageState::Enabled);
                 self.selected_removal = Some(Removal::Recommended);
                 self.selected_list = Some(UadList::All);
-                self.selected_user = Some(User { id: 0, index: 0 });
+                self.selected_user = Some(User::default());
                 Self::filter_package_lists(self);
                 self.loading_state = LoadingState::Ready("".to_string());
                 Command::none()
@@ -216,33 +217,13 @@ impl List {
                         }
                         Command::none()
                     }
-                    RowMessage::ActionPressed => {
-                        let mut commands = vec![];
-                        let actions = action_handler(
-                            &self.selected_user.unwrap(),
-                            &package.into(),
-                            selected_device,
-                            &settings.device,
-                        );
-
-                        for (i, (i_user, action)) in actions.into_iter().enumerate() {
-                            let p_info = PackageInfo {
-                                i_user,
-                                index: i_package,
-                                removal: package.removal.to_string(),
-                            };
-                            // Only the first command can change the package state
-                            commands.push(Command::perform(
-                                perform_adb_commands(action, CommandType::PackageManager(p_info)),
-                                if i == 0 {
-                                    Message::ChangePackageState
-                                } else {
-                                    |_| Message::Nothing
-                                },
-                            ));
-                        }
-                        Command::batch(commands)
-                    }
+                    RowMessage::ActionPressed => Command::batch(build_action_pkg_commands(
+                        &self.selected_user.unwrap(),
+                        package,
+                        selected_device,
+                        &settings.device,
+                        i_package,
+                    )),
                     RowMessage::PackagePressed => {
                         self.description = package.clone().description;
                         package.current = true;
@@ -268,30 +249,13 @@ impl List {
                 }
                 let mut commands = vec![];
                 for i in selected_packages {
-                    let actions = action_handler(
+                    commands.append(&mut build_action_pkg_commands(
                         &self.selected_user.unwrap(),
-                        &(&self.phone_packages[i_user][i]).into(),
+                        &self.phone_packages[i_user][i],
                         selected_device,
                         &settings.device,
-                    );
-
-                    let package = &mut self.phone_packages[i_user][i];
-                    for (j, (i_user, action)) in actions.into_iter().enumerate() {
-                        let p_info = PackageInfo {
-                            i_user,
-                            index: i,
-                            removal: package.removal.to_string(),
-                        };
-                        // Only the first command can change the package state
-                        commands.push(Command::perform(
-                            perform_adb_commands(action, CommandType::PackageManager(p_info)),
-                            if j == 0 {
-                                Message::ChangePackageState
-                            } else {
-                                |_| Message::Nothing
-                            },
-                        ));
-                    }
+                        i,
+                    ));
                 }
                 Command::batch(commands)
             }
@@ -315,7 +279,7 @@ impl List {
                     if !settings.device.multi_user_mode || p.i_user.is_none() {
                         package.state = package.state.opposite(settings.device.disable_mode);
                         package.selected = false;
-                    } else {
+                    } else if !self.phone_packages[p.i_user.unwrap()].is_empty() {
                         self.phone_packages[p.i_user.unwrap()][p.index].state = self.phone_packages
                             [p.i_user.unwrap()][p.index]
                             .state
@@ -604,4 +568,35 @@ fn waiting_view<'a>(
         .center_x()
         .style(style::Container::default())
         .into()
+}
+
+fn build_action_pkg_commands(
+    selected_user: &User,
+    package: &PackageRow,
+    device: &Phone,
+    settings: &DeviceSettings,
+    p_index: usize,
+) -> Vec<Command<Message>> {
+    let actions = action_handler(selected_user, package.into(), device, settings);
+
+    let mut commands = vec![];
+    let mut user_id = None;
+    for (j, (i_user, action)) in actions.into_iter().enumerate() {
+        let p_info = PackageInfo {
+            i_user,
+            index: p_index,
+            removal: package.removal.to_string(),
+        };
+        // Only the first command can change the package state
+        commands.push(Command::perform(
+            perform_adb_commands(action, CommandType::PackageManager(p_info)),
+            if j == 0 || i_user != user_id {
+                Message::ChangePackageState
+            } else {
+                |_| Message::Nothing
+            },
+        ));
+        user_id = i_user
+    }
+    commands
 }

src/gui/views/settings.rs
@@ -4,7 +4,7 @@ use crate::core::save::{
 };
 use crate::core::sync::{get_android_sdk, perform_adb_commands, CommandType, Phone};
 use crate::core::theme::Theme;
-use crate::core::utils::{open_url, string_to_theme, unavailable_users, DisplayablePath};
+use crate::core::utils::{open_url, string_to_theme, DisplayablePath};
 use crate::gui::style;
 use crate::gui::views::list::PackageInfo;
 use crate::gui::widgets::package_row::PackageRow;
@@ -182,11 +182,7 @@ impl Settings {
         }
     }
 
-    pub fn view(
-        &self,
-        packages: &[Vec<PackageRow>],
-        phone: &Phone,
-    ) -> Element<Message, Renderer<Theme>> {
+    pub fn view(&self, phone: &Phone) -> Element<Message, Renderer<Theme>> {
         let radio_btn_theme = Theme::ALL
             .iter()
             .fold(row![].spacing(10), |column, option| {
@@ -242,9 +238,17 @@ impl Settings {
             text("This will not affect the following protected work profile users: ")
                 .size(15)
                 .style(style::Text::Commentary),
-            text(unavailable_users(&phone.user_list, packages).join(", "))
-                .size(15)
-                .style(style::Text::Danger)
+            text(
+                phone
+                    .user_list
+                    .iter()
+                    .filter(|&u| u.protected)
+                    .map(|u| u.id.to_string())
+                    .collect::<Vec<String>>()
+                    .join(", ")
+            )
+            .size(15)
+            .style(style::Text::Danger)
         ];
 
         let multi_user_mode_checkbox = checkbox(