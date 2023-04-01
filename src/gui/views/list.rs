0x192/universal-android-debloater

commit 761fb07f56c70e9b33771df9b2e5891c0168b8d5

feat: warn the user when a work profile is detected (#450)
- display a warning message when switching to a work profile user
- display unavailable work profile users in the settings

src/gui/views/list.rs
@@ -13,7 +13,7 @@ use crate::gui::widgets::package_row::{Message as RowMessage, PackageRow};
 use iced::widget::{
     button, column, container, pick_list, row, scrollable, text, text_input, Space,
 };
-use iced::{Alignment, Command, Element, Length, Renderer};
+use iced::{alignment, Alignment, Command, Element, Length, Renderer};
 
 #[derive(Debug, Default, Clone)]
 pub struct Selection {
@@ -483,16 +483,41 @@ impl List {
                 .spacing(10)
                 .align_items(Alignment::Center);
 
-                let content = column![
-                    control_panel,
-                    packages_scrollable,
-                    description_panel,
-                    action_row,
-                ]
-                .width(Length::Fill)
-                .spacing(10)
-                .align_items(Alignment::Center);
+                let unavailable = container(
+                    column![
+                        text("ADB is not authorized to access this user!").size(22)
+                            .style(style::Text::Danger),
+                        text("The most likely reason is that it is the user of your work profile (also called Secure Folder on Samsung devices). There's really no solution, other than completely disabling your work profile in your device settings.")
+                            .style(style::Text::Commentary)
+                            .horizontal_alignment(alignment::Horizontal::Center),
+                    ]
+                    .spacing(6)
+                    .align_items(Alignment::Center)
+                )
+                .padding(10)
+                .center_x()
+                .style(style::Container::BorderedFrame);
 
+                let content = if !self.phone_packages[self.selected_user.unwrap().index].is_empty()
+                {
+                    column![
+                        control_panel,
+                        packages_scrollable,
+                        description_panel,
+                        action_row,
+                    ]
+                    .width(Length::Fill)
+                    .spacing(10)
+                    .align_items(Alignment::Center)
+                } else {
+                    column![
+                        control_panel,
+                        container(unavailable).height(Length::Fill).center_y(),
+                    ]
+                    .width(Length::Fill)
+                    .spacing(10)
+                    .align_items(Alignment::Center)
+                };
                 container(content).height(Length::Fill).padding(10).into()
             }
         }

src/core/utils.rs
@@ -162,3 +162,21 @@ impl fmt::Display for DisplayablePath {
         write!(f, "{stem}")
     }
 }
+
+pub fn unavailable_users(user_list: &[User], packages: &[Vec<PackageRow>]) -> Vec<String> {
+    let mut unavailable_users = vec![];
+
+    for (u_index, p_list) in packages.iter().enumerate() {
+        if p_list.is_empty() {
+            unavailable_users.push(
+                user_list
+                    .iter()
+                    .find(|u| u.index == u_index)
+                    .unwrap()
+                    .id
+                    .to_string(),
+            );
+        }
+    }
+    unavailable_users
+}

src/gui/mod.rs
@@ -14,7 +14,10 @@ use views::settings::{Message as SettingsMessage, Settings as SettingsView};
 use widgets::navigation_menu::nav_menu;
 
 use iced::widget::column;
-use iced::{window::Settings as Window, Application, Command, Element, Length, Renderer, Settings};
+use iced::{
+    window::Settings as Window, Alignment, Application, Command, Element, Length, Renderer,
+    Settings,
+};
 use std::{env, path::PathBuf};
 
 #[cfg(feature = "self-update")]
@@ -303,12 +306,13 @@ impl Application for UadGui {
                 .map(Message::AboutAction),
             View::Settings => self
                 .settings_view
-                .view(&selected_device)
+                .view(&self.apps_view.phone_packages, &selected_device)
                 .map(Message::SettingsAction),
         };
 
         column![navigation_container, main_container]
             .width(Length::Fill)
+            .align_items(Alignment::Center)
             .into()
     }
 }

src/gui/views/settings.rs
@@ -4,7 +4,7 @@ use crate::core::save::{
 };
 use crate::core::sync::{perform_adb_commands, CommandType, Phone};
 use crate::core::theme::Theme;
-use crate::core::utils::{open_url, string_to_theme, DisplayablePath};
+use crate::core::utils::{open_url, string_to_theme, unavailable_users, DisplayablePath};
 use crate::gui::style;
 use crate::gui::views::list::PackageInfo;
 use crate::gui::widgets::package_row::PackageRow;
@@ -178,7 +178,11 @@ impl Settings {
         }
     }
 
-    pub fn view(&self, phone: &Phone) -> Element<Message, Renderer<Theme>> {
+    pub fn view(
+        &self,
+        packages: &[Vec<PackageRow>],
+        phone: &Phone,
+    ) -> Element<Message, Renderer<Theme>> {
         let radio_btn_theme = Theme::ALL
             .iter()
             .fold(row![].spacing(10), |column, option| {
@@ -230,10 +234,14 @@ impl Settings {
         .width(Length::Fill)
         .style(style::Container::BorderedFrame);
 
-        let multi_user_mode_descr =
-            text("Disabling this setting will typically prevent affecting your work profile")
-                .style(style::Text::Commentary)
-                .size(15);
+        let multi_user_mode_descr = row![
+            text("This will not affect the following protected work profile users: ")
+                .size(15)
+                .style(style::Text::Commentary),
+            text(unavailable_users(&phone.user_list, packages).join(", "))
+                .size(15)
+                .style(style::Text::Danger)
+        ];
 
         let multi_user_mode_checkbox = checkbox(
             "Affect all the users of the phone (not only the selected user)",

commit bf12bf6a5b922aed3d289c1d0fd8565422613860

fix: wrong panel displayed when no device is connected
Regression introduced in 761fb07f56c70e9b33771df9b2e5891c0168b8d5
No public release affected

src/gui/views/list.rs
@@ -423,7 +423,8 @@ impl List {
                 .center_x()
                 .style(style::Container::BorderedFrame);
 
-                let content = if !self.phone_packages[self.selected_user.unwrap().index].is_empty()
+                let content = if selected_device.user_list.is_empty()
+                    || !self.phone_packages[self.selected_user.unwrap().index].is_empty()
                 {
                     column![
                         control_panel,